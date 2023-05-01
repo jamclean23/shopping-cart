313643a7e785aefc54695e6c8abd8277d62527ff
jamclean23 <jamclean23@gmail.com>
2023-05-01 16:37:43 -0400

    Add Homepage can access printify object

--- src/index/React/components/Homepage/Homepage.js
+++ src/index/React/components/Homepage/Homepage.js
@@ -1,6 +1,7 @@
 import './homepage.css';
 import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import accesskey from '../../../accesskey.js';
 
 function Homepage (props) {
     const mountCounter = useRef(0);
@@ -11,10 +12,31 @@ function Homepage (props) {
 
             // Set Title
             document.title = 'My Cool Shop!';
+
+            // Generate cards using data from printify
+            generateCards(accesskey);
         }
 
         mountCounter.current = mountCounter.current + 1;
     }, []);
+
+    async function generateCards () {
+        let printifyObject = await getPrintifyObject(accesskey);
+        console.log(printifyObject);
+    }
+
+    async function getPrintifyObject (accesskey) {
+        let response = await fetch('http://8.40.62.125:49000/', {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                Authorization: 'Bearer ' + accesskey
+            }
+        });
+        const content = await response.json();
+
+        return content;
+    }
 
     return (
         <div className='Homepage'>
