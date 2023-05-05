c0b3edd357bfa3ac35cd3454a2158d10ba267c4e
jamclean23 <jamclean23@gmail.com>
2023-05-05 12:57:38 -0400

    Add cart card creation (blank)

--- src/index/React/components/Cart/Cart.js
+++ src/index/React/components/Cart/Cart.js
@@ -1,12 +1,30 @@
 import './cart.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import uniqid from 'uniqid';
 
 function Cart (props) {
 
+    const [cartContent, setCartContent] = useState();
+
     useEffect(() => {
         document.title = 'My Cool Shop! Cart';
+
+        setCartContent(generateCartContent());        
     }, []);
+
+    function generateCartContent () {
+        let result = [];
+
+        props.cart.forEach((item) => {
+            result.push(
+                <div key={uniqid()} className='cartItem'>
+
+                </div>);
+        });
+
+        return result;
+    }
 
     return (
         <div className='Cart'>
@@ -14,6 +32,9 @@ function Cart (props) {
                 <h1 onClick={props.handleHeroClick}>My Cool Shop!</h1>
                 <Link className='Link' to={props.prefix + '/Cart'}>Cart</Link>
             </header>
+            <main className='cartContainer'>
+                {cartContent}
+            </main>
         </div>
     );
 }
